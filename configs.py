ed805778d1a8a9e52e022de59e8b74750ecb7290
Saurabh81770 <techcroft.info@gmail.com>
2025-07-08 06:21:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+API_ID = int(os.getenv("API_ID", "21974035"))
+API_HASH = os.getenv("API_HASH", "3cb21442fc659e9eb763629a4e1f546b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7616769881:AAE_x1dC0AtP4jr1Wp-oTIyTiD5eLKM5a3w")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://3sonu8177:0FnZW7ZoUvWPNb25@cluster0.yfmr6sp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002844649"))
